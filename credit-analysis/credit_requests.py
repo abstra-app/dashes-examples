d0b86f01229572f0eb8ad48db6a5e6f74e630f45
joaohenrifranco <joaohenrique.franco@gmail.com>
2023-04-06 11:56:26 -0300

    Add credit analysis example

---
+++ credit-analysis/credit_requests.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+request_status = ''
+comments = ''
+esg_score = ''
+legal_assessment = ''
+internal_assessment = ''
+
+data = 'credit_analysis_data.csv'
+df = pd.read_csv(data)
+
+# 
+# 
+# 
+# 
+# 
+# 
